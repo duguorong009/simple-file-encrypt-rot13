462152eb753993aff76a38f81bf125c103895211
duguorong <dudurong009@gmail.com>
2023-01-04 05:08:28 -0800

    Add the utils of "read_file" & "write_file"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Write};
+
+fn main() -> Result<(), Error> {
+    let path = "lines.txt";
+
+    write_file(path, "Rust\nFun")?;
+
+    let buffered = read_file(path)?;
+
+    for line in buffered.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
+}
+
+fn read_file(path: &str) -> Result<BufReader<File>, Error> {
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+    Ok(buffered)
+}
+
+fn write_file(path: &str, content: &str) -> Result<(), Error> {
+    let mut output = File::create(path)?;
+    write!(&mut output, "{}", content)?;
+    Ok(())
 }
