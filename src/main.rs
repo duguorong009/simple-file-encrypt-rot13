c0b280a1c1ac8c779914d13b5a6ac3b5a7398d1c
duguorong <dudurong009@gmail.com>
2023-01-04 05:22:52 -0800

    Add the simple rot13 encryption logic

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Error, Write};
 
 fn main() -> Result<(), Error> {
-    let path = "lines.txt";
+    let plain_file_path = "lines.txt";
 
-    write_file(path, "Rust\nFun")?;
+    let buffered = read_file(plain_file_path)?;
 
-    let buffered = read_file(path)?;
-
+    let mut encrypted: String = "".to_string();
     for line in buffered.lines() {
-        println!("{}", line?);
+        encrypted.push_str(&encrypt_content(line?));
+        encrypted.push_str("\n");
     }
+
+    let enc_file_path = "lines_enc.txt";
+    write_file(enc_file_path, &encrypted)?;
 
     Ok(())
 }
@@ -25,4 +28,8 @@ fn write_file(path: &str, content: &str) -> Result<(), Error> {
     let mut output = File::create(path)?;
     write!(&mut output, "{}", content)?;
     Ok(())
+}
+
+fn encrypt_content(content: String) -> String {
+    rot13::rot13(&content)
 }
